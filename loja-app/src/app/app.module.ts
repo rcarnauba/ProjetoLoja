d97de059f65a64322b1c8601b3ac3e5a57c3e6be
Ricardo Carnaúba e Silva <ricardo.carnauba@fcamara.com.br>
2018-02-05 17:21:27 -0200

    Inicial

---
+++ loja-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProdutoComponent,
+    CarrinhoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
